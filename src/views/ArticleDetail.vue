a5171a85e091d97f3647a749494d82d142312ff3
문준하 <ririro93@gmail.com>
2021-05-18 17:20:01 +0900

    feat: update article view

--- src/views/ArticleDetail.vue
+++ src/views/ArticleDetail.vue
@@ -1,17 +1,40 @@
 <template>
-  <h1>Article Detail</h1>
+  <h1>{{ article.title }}</h1>
+  <p>{{ article.content }}</p>
+  {{ article.comment_set }}
+
+  <hr>
+  <form action="#" @submit="onSubmitCommentCreate">
+    <input type="text" v-model="comment">
+    <input type="submit" value="Add Comment">
+  </form>
+  
+  <hr>
+  
+  <button @click="onDeleteArticle(article.id)">Delete</button>
+
+  <hr>
+
+  <form action="#" @submit="onSubmitArticleUpdate">
+    <input type="text" v-model="updateData.title">
+    <input type="text" v-model="updateData.content">
+    <input type="submit" value="Update Article">
+  </form>
 </template>
 
 <script>
 import axios from 'axios'
 
 import { ref } from 'vue'
-import { useRoute } from 'vue-router'
+import { useRoute, useRouter } from 'vue-router'
 
 export default {
   name: 'ArticleDetail',
   setup() {
+    // Get article info
     const route = useRoute()
+    const router = useRouter()
+
     console.log(route.params)
 
     const article = ref({})
@@ -29,8 +52,46 @@ export default {
         })
     }
 
+    // Delete article
+    const onDeleteArticle = (id) => {
+      axios({
+        method: 'delete',
+        url: `/api/v1/articles/${route.params.id}/`
+      })
+        .then((res) => {
+          console.log(res)
+          article.value = res.data
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    }
+
+    // Update article
+    const updateData = ref({
+      title: '',
+      content: ''
+    })
+    const onSubmitArticleUpdate = () => {
+      axios({
+        method: 'put',
+        url: `/api/v1/articles/${route.params.id}/`,
+        data: updateData.value
+      })
+        .then((res) => {
+          console.log(res)
+          router.push({ name: 'Articles' })
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    }
+
+
     return {
-      article, getArticle
+      article, getArticle,
+      onDeleteArticle,
+      updateData, onSubmitArticleUpdate
     }
   },
   created() {
