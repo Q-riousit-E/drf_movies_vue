9e3093bf1651484b3baab79f5215c3463be1bc69
문준하 <ririro93@gmail.com>
2021-06-18 09:43:36 +0900

    fix: zingchart license cannot be removed in free version

--- src/components/ZingChart.vue
+++ src/components/ZingChart.vue
@@ -16,7 +16,6 @@ export default {
     const store = useStore()
 
     onMounted(() => {
-      ZC.LICENSE = ["569d52cefae586f634c54f86dc99e6a9", "b55b025e438fa8a98e32482b5f768ff5"]
       setTimeout(() => {
         const detailedRatingData = store.state.movies.picked_movie.detailed_ratings_average
         const detailedRatingAverageScores = [
@@ -122,7 +121,4 @@ export default {
   display: none;
 }
 
-#myChart-license-text {
-  display: none !important;
-}
 </style>
